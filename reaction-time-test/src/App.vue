5a786f12cf88a0139aa94ba90f3f3fd66f7739fe
Alysa <devalysarose>
2023-08-17 22:34:45 +0800

    added reaction timer app

---
+++ reaction-time-test/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Reaction Timer Game</h1>
+    <p v-if="showResults">Reaction time: {{ reactionTime }} ms</p>
+    <button @click="startGame" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying"  :delay="delay"  @end="endGame"/>
+  </div>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  components:{ Block },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      reactionTime:0,
+      showResults: false
+    }
+  },
+  methods:{
+    startGame(){
+      this.isPlaying= true
+      this.showResults = false
+      this.delay = 1000 + Math.random() * 5000
+    },
+    endGame(time){
+      this.reactionTime= time
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  display: block;
+    /* display: flex; */
+    /* flex-direction: row; */
+  
+}
+</style>
---
+++ reaction-time-test/src/components/Block.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        <p>Click Me</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['delay'] ,
+    data(){
+        return{
+            showBlock:false,
+            reactionTime:0           
+        }
+    },
+    mounted(){  
+        console.log(this.delay)  
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer()
+        }, this.delay);
+        
+       
+    },
+    methods:{  
+        startTimer(){
+            setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        } ,   
+        stopTimer(){            
+            this.showBlock = false;
+            clearInterval(this.reactionTime);
+            this.$emit('end', this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    height: 200px;
+    background-color: rgb(65, 165, 65);
+    border-radius: 10px;
+    border: 1px solid green;
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+}
+</style>
